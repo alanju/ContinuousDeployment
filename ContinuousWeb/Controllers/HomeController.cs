a5b4b571f2ab9c59162b25150c87d3e54a19f427
Alan Ju (ahju) <ahju@cisco.com>
2017-09-25 15:17:24 +1000

    udpate change

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -15,9 +15,9 @@ namespace ContinuousWeb.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            throw new NotImplementedException();
+            //ViewData["Message"] = "Your application description page.";
+            //return View();
         }
 
         public IActionResult Contact()
